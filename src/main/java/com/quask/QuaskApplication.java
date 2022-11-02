ee118d76858c845e8f58b1c5414fe81f62fc8391
eagle <sasbalazs05@gmail.com>
2022-11-02 15:30:53 +0100

    starter project

---
+++ src/main/java/com/quask/QuaskApplication.java
@@ -0,0 +1,13 @@
+package com.quask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quask/QuaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
